17908137cdc0a082902fbf0d18bf78aa6f61a165
Alvaro Santillan <afsm1995@live.com>
2020-07-15 14:32:28 -0600

    Bug Fixes, HTML Document Refactored.

--- js/custom.js
+++ js/custom.js
@@ -24,19 +24,19 @@ $(document).ready(function () {
 // });
 
 // // Back to top and nav bar animation.
-// $(function () {
-//     $(window).scroll(function () {
-//         if ($(this).scrollTop() < 100) {
-//             // Hide and fade out
-//             $("nav").removeClass("shadow-sm");
-//             $("#back-to-top").fadeOut();
-//         } else {
-//             // Show the fade in
-//             $("nav").addClass("shadow-sm");
-//             $("#back-to-top").fadeIn();
-//         }
-//     });
-// });
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 100) {
+            // Hide and fade out
+            // $("nav").removeClass("shadow-sm");
+            $("#back-to-top").fadeOut();
+        } else {
+            // Show the fade in
+            // $("nav").addClass("shadow-sm");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
 
 /*==================================================================
             HOME
@@ -47,19 +47,19 @@ $(window).scroll(function () {
 });
 
 // Type effect
-$(function () {
-    var typed = new Typed('.typed', {
-        strings: ['| My name is Álvaro F Santillan.', '| I&apos;m a CU Boulder junior majoring in CS (BS).', '| I&apos;m a CU Boulder buff minoring in TAM (BS).', '| I&apos;m an aspiring mobile application developer.', '| I&apos;m an aspiring full-stack developer.', '| I have experience making iOS and Android apps.', '| I have experience making well-designed websites.', '| I have experience with Python and Python libraries.', '| I have experience with Swift, Xcode, Java, and Android Studios.', '| I work with the professionals around me.', '| I build on the giants before me.', '| I like to design and create software.', '| Let&apos;s create something great together.'],
-        // strings: ['| My name is Álvaro F Santillan.', '| I have experience with Swift, Xcode, Java, and Android Studios.'],
-        startDelay: 3000,
-        typeSpeed: 45,
-        backSpeed: 20,
-        backDelay: 1000,
-        loop: true,
-        loopCount: 1,
-        smartBackspace: true
-    });
-});
+// $(function () {
+//     var typed = new Typed('.typed', {
+//         strings: ['| My name is Álvaro F Santillan.', '| I&apos;m a CU Boulder junior majoring in CS (BS).', '| I&apos;m a CU Boulder buff minoring in TAM (BS).', '| I&apos;m an aspiring mobile application developer.', '| I&apos;m an aspiring full-stack developer.', '| I have experience making iOS and Android apps.', '| I have experience making well-designed websites.', '| I have experience with Python and Python libraries.', '| I have experience with Swift, Xcode, Java, and Android Studios.', '| I work with the professionals around me.', '| I build on the giants before me.', '| I like to design and create software.', '| Let&apos;s create something great together.'],
+//         // strings: ['| My name is Álvaro F Santillan.', '| I have experience with Swift, Xcode, Java, and Android Studios.'],
+//         startDelay: 3000,
+//         typeSpeed: 45,
+//         backSpeed: 20,
+//         backDelay: 1000,
+//         loop: true,
+//         loopCount: 1,
+//         smartBackspace: true
+//     });
+// });
 
 /*==================================================================
             CONTACT
