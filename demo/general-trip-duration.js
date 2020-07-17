6e5625f6ded65b205040192f3c41a131c6f792c5
Alvaro Santillan <afsm1995@live.com>
2020-07-17 14:34:24 -0600

    Longest, Shortest, & Average Trip By Duration (Min) Chart Added.

--- demo/general-trip-duration.js
+++ demo/general-trip-duration.js
@@ -2,18 +2,36 @@
 Chart.defaults.global.defaultFontFamily = 'Poppins', 'sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-// Pie Chart Example
 var ctx = document.getElementById("generalTripTypeDuration");
 var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
+  type: 'bar',
   data: {
-    labels: ["UberX", "Uber Pool", "Express Pool"],
-    datasets: [{
-      data: [78.11, 12.07, 9.81],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
+    labels: ["Longest Trip", "Shortest Trip", "Average Trip"],
+      datasets: [{
+        label: 'All',
+        backgroundColor: '#4e73df',
+        hoverBackgroundColor: '#2e59d9',
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+        data: [121.12, 1.2, 14.77]
+      }, {
+        label: 'UberX',
+        backgroundColor: '#1cc88a',
+        hoverBackgroundColor: '#1cc88a',
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+        data: [121.12, 1.2, 14.32]
+      }, {
+        label: 'Pool',
+        backgroundColor: '#36b9cc',
+        hoverBackgroundColor: '#2c9faf',
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+        data: [59.90, 2.37, 16.48]
+      }, {
+        label: 'Express Pool',
+        backgroundColor: '#1cc88a',
+        hoverBackgroundColor: '#1cc88a',
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+        data: [69.3, 1.55, 16.58]
+      }]
   }, 
   options: {
     maintainAspectRatio: false,
