a16623ae35be60eb93fc4560510e3b5fa6d32337
Alvaro Santillan <afsm1995@live.com>
2020-07-18 13:41:10 -0600

    Added Two Charts To Zip Code Section.

---
+++ demo/zip-top-five-most-popular.js
@@ -0,0 +1,36 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Poppins', 'sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+var ctx = document.getElementById("zipTopFive");
+var myPieChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["80202 (DownTown Denver)", "80205 (Denver)", "80211 (Denver)", "80203 (DownTown Denver)", "80204 (DownTown Denver)"],
+      datasets: [{
+        label: 'Trips',
+        backgroundColor: '#4e73df',
+        hoverBackgroundColor: '#2e59d9',
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+        data: [687, 508, 394, 297, 243]
+      }]
+  }, 
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false,
+      // position: 'bottom'
+    },
+    cutoutPercentage: 60,
+  },
+});
