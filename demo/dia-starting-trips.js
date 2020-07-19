ab85775a91f0f10477cb57a9ebf788f893dde6b6
Alvaro Santillan <afsm1995@live.com>
2020-07-18 22:51:08 -0600

    Added 2 Charts To DIA Section.

---
+++ demo/dia-starting-trips.js
@@ -0,0 +1,42 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Poppins', 'sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("diaStartingTrips");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    
+    datasets: [{
+      data: [0, 61, 4, 0],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }, {
+      data: [65],
+      backgroundColor: ['#4e73df'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+    labels: ["Trips Starting At DIA", "UberX", "Pool", "Express Pool"]
+  }, 
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false,
+      // position: 'bottom'
+    },
+    cutoutPercentage: 60,
+  },
+});
