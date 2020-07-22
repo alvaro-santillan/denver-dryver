84f2f8b2c6c9dc7fc70b08067910ef6adeac1a21
Alvaro Santillan <afsm1995@live.com>
2020-07-22 12:27:33 -0600

    Combined 2 Charts In General Section.

--- demo/general-trip-profit-breakdown.js
+++ demo/general-trip-profit-breakdown.js
@@ -7,13 +7,18 @@ var ctx = document.getElementById("generalTripProfitBreakdown");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Fair", "Promotion", "Tip", "Share Adjustment", "Tolls"],
     datasets: [{
       data: [90.12, 1.42, 7.76, 0.05, 0.57],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }, {
+      data: [0, 0, 0, 0, 0, 2.15, 6.33, 0.26, 53.71, 3.5, 7.99, 0.73, 0.35, 24.49, 0.5],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
+    labels: ["Fair", "Promotion", "Tip", "Share Adjustment", "Tolls", "Cancellation Fees", "Base", "Additional Pickups", "Distance", "Fare Supplement", "Surge", "Long Pickup Distance", "Long Pickup Time", "Trip Time", "Wait Time"],
   }, 
   options: {
     maintainAspectRatio: false,
