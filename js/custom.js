45351187417d0b3c8826a0627632bd4abd630851
Alvaro Santillan <afsm1995@live.com>
2020-07-23 15:48:59 -0600

    Side Menu Is Now Static.

--- js/custom.js
+++ js/custom.js
@@ -13,9 +13,15 @@ $(function () {
             SIDE BAR & BACK TO TOP BUTTON
 ==================================================================*/
 $(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
+  $("#sidebar").mCustomScrollbar({
+      theme: "minimal"
+  });
+
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar, #content').toggleClass('active');
+      $('.collapse.in').toggleClass('in');
+      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
 });
 
 // // Back to top and nav bar animation.
