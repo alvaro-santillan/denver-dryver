b281d96d08d21ebc616df6daae54c3d50aeb3d11
Alvaro Santillan <afsm1995@live.com>
2020-07-23 16:41:01 -0600

    Updated Colors For 3 Charts.

--- demo/canceled-profitable-canceled-trip.js
+++ demo/canceled-profitable-canceled-trip.js
@@ -9,14 +9,14 @@ var myPieChart = new Chart(ctx, {
     labels: ["All Trips", "UberX", "Pool", "Express Pool"],
     datasets: [{
       label: "Profitable Canceled Trip",
-      backgroundColor: "rgba(154,178,96,0.5)",
-      hoverBackgroundColor: "rgba(154,178,96,1)",
+      backgroundColor: ['rgba(26, 26, 26, 1)', 'rgba(3, 42, 102, 1)', 'rgba(189, 17, 52, 1)', 'rgba(254, 213, 48, 1)'],
+      hoverBackgroundColor: ['#000000', '#00114D', '#A4001B', '#E5BC17'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
       data: [4.67, 3.25, 8.21, 11.67],
     }, {
       label: "Non-Profitable Canceled Trip",
-      backgroundColor: "rgba(197,213,167,0.5)",
-      hoverBackgroundColor: "rgba(197,213,167,1)",
+      backgroundColor: ['rgba(26, 26, 26, 0.2)', 'rgba(3, 42, 102, 0.2)', 'rgba(189, 17, 52, 0.2)', 'rgba(254, 213, 48, 0.2)'],
+      hoverBackgroundColor: ['rgba(26, 26, 26, 0.3)', 'rgba(3, 42, 102, 0.3)', 'rgba(189, 17, 52, 0.3)', 'rgba(254, 213, 48, 0.3)'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
       data: [95.33, 96.75, 91.79, 88.33]
     }]
