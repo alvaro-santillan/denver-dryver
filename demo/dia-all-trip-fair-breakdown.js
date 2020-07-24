5ac4758c965c28c4ab1a983b96ce6bbce2937ffd
Alvaro Santillan <afsm1995@live.com>
2020-07-24 13:13:53 -0600

    Updated Colors On 4 Charts In DIA Section.

--- demo/dia-all-trip-fair-breakdown.js
+++ demo/dia-all-trip-fair-breakdown.js
@@ -10,13 +10,13 @@ var myPieChart = new Chart(ctx, {
     
     datasets: [{
       data: [0, 0.66, 7.4, 1.95, 0.05, 67.17, 0.0, 3.11, 0.33, 0.12, 18.9, 0.32],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      backgroundColor: ['#FED530', '#BD1134', '#032A66', '#1A1A1A', '#c62828', '#AD1457', '#6A1B9A', '#4527A0', '#283593', '#1565C0', '#0277BD', '#00838F', '#00695C', '#2E7D32', '#558B2F'],
+      hoverBackgroundColor: ['#E5BC17', '#A4001B', '#00114D', '##000000', '#AD0F0F', '#94003E', '#510281', '#2C0E87', '#0F1C7A', '#004CA7', '#005EA4', '#006A76', '#005043', '#156419', '#3C7216'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }, {
       data: [91.95, 0.66, 7.4],
-      backgroundColor: ['#4e73df'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      backgroundColor: ['#FED530', '#BD1134', '#032A66', '#1A1A1A', '#c62828', '#AD1457', '#6A1B9A', '#4527A0', '#283593', '#1565C0', '#0277BD', '#00838F', '#00695C', '#2E7D32', '#558B2F'],
+      hoverBackgroundColor: ['#E5BC17', '#A4001B', '#00114D', '##000000', '#AD0F0F', '#94003E', '#510281', '#2C0E87', '#0F1C7A', '#004CA7', '#005EA4', '#006A76', '#005043', '#156419', '#3C7216'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
     labels: ["Fair", "Promotion", "Tip", "Base", "Additional Pickups", "Distance", "Fare Supplement", "Surge", "Long Pickup Distance", "Long Pickup Time", "Trip Time", "Wait Time"]
--- demo/dia-express-trip-fair-breakdown.js
+++ demo/dia-express-trip-fair-breakdown.js
@@ -10,13 +10,13 @@ var myPieChart = new Chart(ctx, {
     
     datasets: [{
       data: [0, 0.92, 0.0, 2.5, 0.53, 69.1, 0.0, 0.0, 0.0, 0.0, 26.95, 0.0, 0.92, 0.0],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      backgroundColor: ['#FED530', '#BD1134', '#032A66', '#1A1A1A', '#c62828', '#AD1457', '#6A1B9A', '#4527A0', '#283593', '#1565C0', '#0277BD', '#00838F', '#00695C', '#2E7D32', '#558B2F'],
+      hoverBackgroundColor: ['#E5BC17', '#A4001B', '#00114D', '##000000', '#AD0F0F', '#94003E', '#510281', '#2C0E87', '#0F1C7A', '#004CA7', '#005EA4', '#006A76', '#005043', '#156419', '#3C7216'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }, {
       data: [99.08, 0.92, 0.0],
-      backgroundColor: ['#4e73df'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      backgroundColor: ['#FED530', '#BD1134', '#032A66', '#1A1A1A', '#c62828', '#AD1457', '#6A1B9A', '#4527A0', '#283593', '#1565C0', '#0277BD', '#00838F', '#00695C', '#2E7D32', '#558B2F'],
+      hoverBackgroundColor: ['#E5BC17', '#A4001B', '#00114D', '##000000', '#AD0F0F', '#94003E', '#510281', '#2C0E87', '#0F1C7A', '#004CA7', '#005EA4', '#006A76', '#005043', '#156419', '#3C7216'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
     labels: ["Fair", "Promotion", "Tip", "Base", "Additional Pickups", "Distance", "Fare Supplement", "Surge", "Long Pickup Distance", "Long Pickup Time", "Trip Time", "Wait Time"]
